8b08886bde99e2996e6964e63a129e233ba16cde
Merle Makoben <merlemakoben@gmail.com>
2022-03-08 12:46:19 +0100

    Add Button component

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import "./Button.css";
+export default function Button({ children, onClick, isDark }) {
+  return (
+    <button
+      className={isDark ? "Button Button--dark" : "Button"}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
